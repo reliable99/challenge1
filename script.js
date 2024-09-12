f6179db12c1b9e96585ca60a58fc19b2b2434315
relaible99 <abdulmuhminsodiq@gmail.com>
2024-09-12 18:28:25 +0100

    isvalid email

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
 
-function showError (input, message){
-    const formControl = input.parentElement;
-    formControl.className = "form-control error"
+// show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
 }
 
+// show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+// check email is valid
 
-
+function isValidEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLocaleLowerCase());
+}
 // event listners
-form.addEventListener('submit', function(e) {
-    e.preventDefault()    
-    if (username.value=== '') {
-        showError(username, 'Username is required')
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    }else{
-        showSuccess(username)
-    }
-})
+  if (username.value === "") {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }
+  if (email.value === "") {
+    showError(email, "Email  is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is not valid");
+  } else {
+    showSuccess(email);
+  }
+  if (password.value === "") {
+    showError(password, "Password  is required");
+  } else {
+    showSuccess(password);
+  }
+  if (password2.value === "") {
+    showError(password2, "Password  is required");
+  } else {
+    showSuccess(password2);
+  }
+});
